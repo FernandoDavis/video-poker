8ac0fba28b4fbaceaafb2552cf09e1c9dbbc27dd
Fernando Davis <fernando.davis@upr.edu>
2017-11-04 17:54:56 -0400

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -3,14 +3,6 @@ import java.util.ArrayList;
 
 public class Card {
     public static void main(String[] args) {
-
-        List<String> cards = new ArrayList<>();
-        Deck deck = new Deck();
-        deck.shuffle();
-
-        Hand hand = new Hand();
-        hand.getHand(cards);
-        hand.showHand();
-
+        
     }
 }
