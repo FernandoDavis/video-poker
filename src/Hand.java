304ce91130e3fae46c6194be2210b1e39fe985ae
Fernando Davis <fernando.davis@upr.edu>
2017-11-04 17:55:48 -0400

    Update Hand.java

--- src/Hand.java
+++ src/Hand.java
@@ -3,12 +3,4 @@ import java.util.Random;
 
 public class Hand{
 
-    Random random = new Random();
-
-    // select 5 cards to distribute to player
-
-    // show player hand
-    public void showHand() {
-    
-    }
 }
